da3a3852c6d9fd7bae59caecf2174d19675c9a1f
Andrey <DENANDREY191@GMAIL.COM>
2023-06-20 20:05:14 +0200

    init: start project

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyAb2MvZEqUq6rZ4vm59yoruXGlDJpoee14",
+  authDomain: "hotel-reg-25a76.firebaseapp.com",
+  projectId: "hotel-reg-25a76",
+  storageBucket: "hotel-reg-25a76.appspot.com",
+  messagingSenderId: "433505453552",
+  appId: "1:433505453552:web:0e77c25c25f6af815a4935"
+};
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import middleware from './middleware';
+export default createStore(
+  rootReducer,  composeWithDevTools(applyMiddleware(middleware)),
+);
